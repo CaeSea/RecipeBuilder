99f129cfdec950c570d55a93bfd47419cb723841
Caeron Seabourne <caeking93@gmail.com>
2020-01-22 19:36:00 +0000

    Added and fleshed out components

--- src/app/components/shopping-list/shopping-list.component.ts
+++ src/app/components/shopping-list/shopping-list.component.ts
@@ -1,11 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Ingredient } from '../../models/ingredients.model';
+
 @Component({
   selector: 'rb-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.scss']
 })
 export class ShoppingListComponent implements OnInit {
+
+  protected ingredients = [
+    new Ingredient('Apples', 5),
+    new Ingredient('Tomatoes', 10)
+  ];
 
   constructor() { }
 
